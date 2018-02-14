2ea7fe1cad4838a542b45ee4269e3ad1089a7dac
Jake Marrapode <jake.marrapode@gmail.com>
2018-02-14 16:35:20 -0700

    closes #96

--- server/src/main/java/com/tripco/t09/planner/Place.java
+++ server/src/main/java/com/tripco/t09/planner/Place.java
@@ -5,9 +5,83 @@ package com.tripco.t09.planner;
  * There may be other attributes of a place, but these are required to plan a trip.
  */
 public class Place {
+
   public String id;
   public String name;
   public String latitude;
   public String longitude;
+
+  public double numLatitude;
+  public double numLongitude;
+
+  /**
+   * Takes a single string coordinate of various types, if the coordinate is already in
+   *  proper decimal format it will simply return. Otherwise the method will call
+   *   stringToCoordinate to convert the coordinate.
+   * @params String containing the coordinate
+   * @return double containing the string coordinate converted to decimal coordinate
+   */
+  public double convertCoordinate(String value) {
+    double result;
+
+    try {
+      result = Double.parseDouble(value);
+    } catch (NumberFormatException e) {
+      /*Coordinate is not already in number format*/
+      result = stringToCoordinate(value);
+    }
+
+  return result;
+
+  }
+
+  /**
+   * Takes a single string coordinate of various types and converts it to a decimal value.
+   *  Assumes that the string is of a correct coordinate input type.
+   * @params String containing the coordinate
+   * @return double containing the string coordinate converted to decimal coordinate
+   */
+  public double stringToCoordinate(String value){
+    String coordinate;
+    double degrees = 0;
+    double minutes = 0;
+    double seconds = 0;
+    double result;
+
+
+    //Replaces all degrees, minutes, and seconds symbols with empty spaces to allow the
+    // string to be parsed into double values.
+
+    coordinate = value.replaceAll("[°\'\"′″]", " " );
+
+    int counter = 0;
+    int last = 0;
+
+    // Parses the new string and reads in the values of the coordinates
+    for(int i = 0; i < coordinate.length(); i++){
+      if(coordinate.charAt(i) == ' ' && last != i){
+        if(counter == 0) {
+          degrees = Double.parseDouble(coordinate.substring(last, i));
+          last = i+1;
+          counter++;
+        } else if(counter == 1){
+          minutes = Double.parseDouble(coordinate.substring(last, i));
+          last = i+1;
+          counter++;
+        } else if(counter == 2){
+          seconds = Double.parseDouble(coordinate.substring(last, i));
+          last = i+1;
+          counter++;
+        }
+      }
+    }
+
+    // Checks to see if the value should be negated
+    if(coordinate.indexOf('S') != -1 || coordinate.indexOf('W') != -1){
+      return -1 * (double)Math.round((degrees + (minutes / 60) + (seconds / 3600)) * 100000d) / 100000d;
+    }
+
+    return (double)Math.round((degrees + (minutes / 60) + (seconds / 3600)) * 100000d) / 100000d;
+  }
 
 }
---
+++ server/src/test/java/com/tripco/t09/planner/TestPlace.java
@@ -0,0 +1,43 @@
+package com.tripco.t09.planner;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
+/*
+  This class contains tests for the Place class.
+ */
+@RunWith(JUnit4.class)
+public class TestPlace {
+  Place place;
+
+  // Setup to be done before every test in TestPlace
+  @Before
+  public void initialize() {
+    place = new Place();
+  }
+
+
+  @Test
+  public void testTrue() {
+    // assertTrue checks if a statement is true
+    assertTrue(true == true);
+  }
+
+  @Test
+  public void testConvertCoordinate() {
+    assert(10.8333 == place.convertCoordinate("10.8333"));
+  }
+
+  @Test
+  public void testStringToCoordinate() {
+    assert(15.03417 == place.stringToCoordinate("15°2\'3\"N"));
+  }
+
+}
