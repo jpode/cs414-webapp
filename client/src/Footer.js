1dbf0ad784ce6fc9085f9b0cd30416165f497dda
SoDakHibida <jjserva@rams.colostate.edu>
2018-03-01 15:20:27 -0700

    Addition of Branding png images

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -10,8 +10,15 @@ class Footer extends Component {
 
   render() {
     return (
-        <div id="footer" className="jumbotron">
-          <h4>© TripCo t{this.props.number} {this.props.name} 2018</h4>
+        <div id="footer" className="jumbotron container-fluid">
+                <div className="row">
+                    <div className="col-8 align-self-center">
+                        <h4 className="text-white">© TripCo t{this.props.number} {this.props.name} 2018</h4>
+                    </div>
+                    <div className="col-4">
+                        <img src="https://raw.githubusercontent.com/csu314sp18/tripco/setup/sprint3/CSU-Official-wrdmrk-357-617_Rev.png?token=Abgsf1PkOTR0A9RK8uV-N1wwo6z-d3Qrks5aoHjawA%3D%3D" className="img-fluid" alt="CSU"/>
+                    </div>
+                </div>
         </div>
     )
   }
--- client/src/Header.js
+++ client/src/Header.js
@@ -10,16 +10,17 @@ class Header extends Component{
   render() {
     return(
         <div id="header" className="jumbotron">
+          <img src="https://raw.githubusercontent.com/csu314sp18/tripco/setup/sprint3/CompSci-NS-CSU-1-Hrev.png?token=Abgsf3eBaZyE1_KYpWDtuhuwefmoXOGVks5aoHl8wA%3D%3D" className="img-fluid" alt="CSU CS Dept."/>
           {this.title()}
-          <p className="lead">"Want to travel far and wide?"</p>
+          <p className="lead text-white">"Want to travel far and wide?"</p>
             <ol >
-              <li>
+              <li className="text-white">
                 Choose options for trip planning, information to display about locations,
                 and how the trip map and itinerary should be saved.</li>
-              <li>
+              <li className="text-white">
                 Choose your destinations by loading existing sets of destinations or
                 find more in an extensive database of locations worldwide.</li>
-              <li>
+              <li className="text-white">
                 Plan the trip with the options you selected.
                 Review and revise the trip origin and order.
                 Save the trip map and itinerary for future reference.</li>
@@ -29,7 +30,7 @@ class Header extends Component{
   }
 
   title() {
-    return( <h3>TripCo <small>t{this.props.number} {this.props.name}</small></h3> )
+    return( <h3 className="text-white">TripCo <small>t{this.props.number} {this.props.name}</small></h3> )
   }
 }
 
