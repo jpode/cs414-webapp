b407de34c141ea123b89e0a1cfec6019ecc57591
lburford <lburford@comcast.net>
2018-03-21 18:12:25 -0600

    Re-designing for style guide.

--- client/src/Destinations.js
+++ client/src/Destinations.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Query from './Query';
 
 /* Destinations reside in the parent object so they may be shared
  * with the Trip object.
@@ -44,38 +45,7 @@ class Destinations extends Component {
             </div>
             <h5>There are {this.props.trip.places.length} destinations. </h5>
             <hr/>
-            <label>
-              Search destinations from a database:
-              <input type="checkbox" name="searchCheck" value="on"/>
-            </label>
-            <div className="input-group" role="group">
-            <input type="text" className="form-control" onChange = {this.handleSubmit} placeholder="Find"/>
-            <span className="input-group-btn">
-              <button className="btn btn-primary " type="button">Search</button>
-            </span>
-            </div>
-            <table className="table table-responsive table-bordered">
-              <thead>
-              <tr className="table-info">
-                <th className="align-middle">ID</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                <th className="table-info align-middle">Name</th>
-              </tr>
-              </tbody>
-              <tbody>
-              <tr>
-                <th className="table-info align-middle">Municipality</th>
-              </tr>
-              </tbody>
-              <tbody>
-              <tr>
-                <th className="table-info align-middle">Add</th>
-              </tr>
-              </tbody>
-            </table>
+            <Query/>
           </div>
         </div>
     )
