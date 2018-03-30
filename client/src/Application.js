e59bd071aeae673e53114d827fbff1448dc855f3
lburford <lburford@comcast.net>
2018-03-30 13:14:44 -0600

    adds the custom.scsss import

--- client/src/Application.js
+++ client/src/Application.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Options from './Options';
 import Destinations from './Destinations';
 import Trip from './Trip';
+import "./custom.scss";
 
 /* Renders the application.
  * Holds the destinations and options state shared with the trip.
