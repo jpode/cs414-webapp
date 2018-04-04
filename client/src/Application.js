aa9b83d915c89aac506f8e43eec1b4e8f0b51520
Jake Marrapode <jake.marrapode@gmail.com>
2018-04-04 15:56:49 -0600

    branding

--- client/src/Application.js
+++ client/src/Application.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Options from './Options';
 import Destinations from './Destinations';
 import Trip from './Trip';
-//import "./custom.scss";
+import "./custom.scss";
 
 /* Renders the application.
  * Holds the destinations and options state shared with the trip.
