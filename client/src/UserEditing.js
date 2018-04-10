77f5b467923143993fc63513fe559617405e8544
Jake Marrapode <jake.marrapode@gmail.com>
2018-04-10 13:50:56 -0600

    initial

--- client/src/UserEditing.js
+++ client/src/UserEditing.js
@@ -4,7 +4,10 @@ class UserEditing extends Component {
   constructor(props) {
     super(props);
     this.state={
-      selected: 0,
+      insertSelected: 0,
+      removeSelected: 0,
+      reverseSelected: 0,
+      newStartSelected: 0,
       editType: "",
       targetIndex: 0,
       destIndex: -1,
@@ -16,22 +19,55 @@ class UserEditing extends Component {
       }
     }
 
-    this.handleCreation = this.handleCreation.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+
+    this.insert = this.insert.bind(this);
     this.handleCustomName = this.handleCustomName.bind(this);
     this.handleCustomLongitude = this.handleCustomLongitude.bind(this);
     this.handleCustomLatitude = this.handleCustomLatitude.bind(this);
     this.handleCustomOrder = this.handleCustomOrder.bind(this);
-    this.handleInsert = this.handleInsert.bind(this);
+
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleReverse = this.handleReverse.bind(this);
+    this.handleNewStart = this.handleNewStart.bind(this);
+
 
   }
 
-  handleCreation(event){
-    this.setState({editType : "insert"});
-    if(this.state.selected === 0){
-      this.setState({selected : 1});
-    } else {
-      this.setState({selected : 0});
+  handleSelect(arg){
+    this.setState({editType : arg});
+    if(arg === "insert") {
+      if (this.state.insertSelected === 0) {
+        this.setState({insertSelected: 1});
+      } else {
+        this.setState({insertSelected: 0});
+      }
     }
+    if(arg === "remove") {
+      if (this.state.removeSelected === 0) {
+        this.setState({removeSelected: 1});
+      } else {
+        this.setState({removeSelected: 0});
+      }
+    }
+    if(arg === "reverse") {
+      if (this.state.reverseSelected === 0) {
+        this.setState({reverseSelected: 1});
+      } else {
+        this.setState({reverseSelected: 0});
+      }
+    }
+    if(arg === "newStart") {
+      if (this.state.newStartSelected === 0) {
+        this.setState({newStartSelected: 1});
+      } else {
+        this.setState({newStartSelected: 0});
+      }
+    }
+  }
+
+  insert(){
+    this.props.editTrip(this.state);
   }
 
   handleCustomName(event){
@@ -58,8 +94,16 @@ class UserEditing extends Component {
     this.setState({destIndex: event.target.value});
   }
 
-  handleInsert(){
-    this.props.editTrip(this.state);
+  handleRemove(event){
+    alert("Not implemented");
+  }
+
+  handleReverse(event){
+    alert("Not implemented");
+  }
+
+  handleNewStart(event){
+    alert("Not implemented");
   }
 
   render(){
@@ -75,12 +119,21 @@ class UserEditing extends Component {
         </div>
 
         <div className="btn-group btn-group-toggle" data-toggle="buttons">
-          <label className={"btn btn-outline-dark btn-success".concat((this.state.selected === 1) ? " active" : "")}>
-            <input type="radio" id="new_place" name="new_place" value="on" onClick={this.handleCreation} />Create New Destination
+          <label className={"btn btn-outline-dark btn-success".concat((this.state.insertSelected === 1) ? " active" : "")}>
+            <input type="radio" id="new_place" name="new_place" value="on" onClick={this.handleSelect("insert")} />Create New Destination
+          </label>
+          <label className={"btn btn-outline-dark btn-success".concat((this.state.removeSelected === 1) ? " active" : "")}>
+            <input type="radio" id="remove" name="remove" value="on" onClick={this.handleSelect("remove")} />Remove A Place
+          </label>
+          <label className={"btn btn-outline-dark btn-success".concat((this.state.reverseSelected === 1) ? " active" : "")}>
+            <input type="radio" id="reverse" name="reverse" value="on" onClick={this.handleSelect("reverse")} />Reverse The Trip
+          </label>
+          <label className={"btn btn-outline-dark btn-success".concat((this.state.newStartSelected === 1) ? " active" : "")}>
+            <input type="radio" id="newStart" name="newStart" value="on" onClick={this.handleSelect("newStart")} />Select New Start Location
           </label>
         </div>
 
-        {this.state.selected === 1 &&
+        {this.state.insertSelected === 1 &&
           <div className="input-group" role="group">
             <span>
               <input type="text" className="form-control" onChange={this.handleCustomName} placeholder="Destination Name"/>
