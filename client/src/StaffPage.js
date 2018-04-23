47018edc41034f0a66465c806818c69bfc815682
lburford <lburford@comcast.net>
2018-04-23 15:37:09 -0600

    Adds js page for staff bios

---
+++ client/src/StaffPage.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class StaffPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+        <div>
+          <p>
+            Hello This is the Staff Page!
+          </p>
+        </div>
+    )
+  }
+
+}
+
+export default StaffPage;
